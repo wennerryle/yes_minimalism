6ebdfe185827c2bd7a069ae9d892d20507397210
wennerryle <wennerryle@gmail.com>
2023-09-22 17:15:29 +0300

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -20,5 +19,3 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [daisyui],
+}
